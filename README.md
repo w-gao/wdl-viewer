e4f7644e8ab5c3e72463db56fe455d6fcd69dda8
William Gao <wlgao@ucsc.edu>
2022-04-30 20:24:24 -0700

    Pull in react-flow

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wdl-viewer
 
+A web-based visualization tool to view (inter and inner) dependencies of WDL workflows.
+
+
+## Available commands
+
+#### `yarn dev`
+
+Run the dev server on http://localhost:3000. Changes to the source code will
+automatically be reloaded in the browser.
+
+
+## License
+
+Copyright (C) 2022 William Gao, under the MIT license.
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,162 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import ReactFlow, {
+    addEdge,
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    ReactFlowInstance,
+    MarkerType,
+    Connection
+} from 'react-flow-renderer';
+import './App.scss'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export const initialNodes = [
+    {
+        id: '1',
+        type: 'input',
+        data: {
+            label: (
+                <>
+                    Welcome to <strong>React Flow!</strong>
+                </>
+            ),
+        },
+        position: { x: 250, y: 0 },
+    },
+    {
+        id: '2',
+        data: {
+            label: (
+                <>
+                    This is a <strong>default node</strong>
+                </>
+            ),
+        },
+        position: { x: 100, y: 100 },
+    },
+    {
+        id: '3',
+        data: {
+            label: (
+                <>
+                    This one has a <strong>custom style</strong>
+                </>
+            ),
+        },
+        position: { x: 400, y: 100 },
+        style: {
+            background: '#D6D5E6',
+            color: '#333',
+            border: '1px solid #222138',
+            width: 180,
+        },
+    },
+    {
+        id: '4',
+        position: { x: 250, y: 200 },
+        data: {
+            label: 'Another default node',
+        },
+    },
+    {
+        id: '5',
+        data: {
+            label: 'Node id: 5',
+        },
+        position: { x: 250, y: 325 },
+    },
+    {
+        id: '6',
+        type: 'output',
+        data: {
+            label: (
+                <>
+                    An <strong>output node</strong>
+                </>
+            ),
+        },
+        position: { x: 100, y: 480 },
+    },
+    {
+        id: '7',
+        type: 'output',
+        data: { label: 'Another output node' },
+        position: { x: 400, y: 450 },
+    },
+];
+
+export const initialEdges = [
+    { id: 'e1-2', source: '1', target: '2', label: 'this is an edge label' },
+    { id: 'e1-3', source: '1', target: '3' },
+    {
+        id: 'e3-4',
+        source: '3',
+        target: '4',
+        animated: true,
+        label: 'animated edge',
+    },
+    {
+        id: 'e4-5',
+        source: '4',
+        target: '5',
+        label: 'edge with arrow head',
+        markerEnd: {
+            type: MarkerType.ArrowClosed,
+        },
+    },
+    {
+        id: 'e5-6',
+        source: '5',
+        target: '6',
+        type: 'smoothstep',
+        label: 'smooth step edge',
+    },
+    {
+        id: 'e5-7',
+        source: '5',
+        target: '7',
+        type: 'step',
+        style: { stroke: '#f6ab6c' },
+        label: 'a step edge',
+        animated: true,
+        labelStyle: { fill: '#f6ab6c', fontWeight: 700 },
+    },
+];
+
+
+const onInit = (instance: ReactFlowInstance) => console.log('flow loaded:', instance);
+
+const OverviewFlow = () => {
+    const [nodes, setNodes, onNodesChange] = useNodesState<any>(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState<any>(initialEdges);
+    const onConnect = (params: Connection) => setEdges((edges) => addEdge(params, edges));
+
+    return (
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={onInit}
+            fitView
+            attributionPosition="bottom-right"
+        >
+            <MiniMap />
+            <Controls />
+            <Background color="#aaa" gap={16} />
+        </ReactFlow>
+    );
+};
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      <div className="App">
+          <OverviewFlow />
+      </div>
   )
 }
 
