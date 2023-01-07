6fe7b31b46c69a369602b830056d6760927dfd3d
William Gao <williamgao520@gmail.com>
2023-01-06 22:08:42 -0800

    Set up `react-flow`

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import HomeView from "./views/home.view";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HomeView />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
